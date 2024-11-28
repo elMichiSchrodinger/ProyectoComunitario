ac64fa7fe88e2b4a38cf1d841c705b8120ae7a79
0Deiby0 <deiby.garcia.y@uni.pe>
2024-11-28 15:28:27 -0500

    a

---
+++ FrontendDBD/frontend/src/app/componentreclutamiento/lista-vacantes/detalle-vacante/detalle-vacante.component.ts
@@ -0,0 +1,34 @@
+import {Component, OnInit} from '@angular/core';
+import {VacanteResumidaModel} from '../../../models/modelsreclutamiento/vacanteResumida.model';
+import {Vacante} from '../../../models/modelsreclutamiento/vacante.model';
+import {ListaVacantesService} from '../../../services/servicereclutamiento/lista-vacantes.service';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-detalle-vacante',
+  standalone: true,
+  imports: [],
+  templateUrl: './detalle-vacante.component.html',
+  styleUrl: './detalle-vacante.component.css'
+})
+export class DetalleVacanteComponent implements OnInit {
+  detalleVacante!:Vacante;
+  constructor(private vacanteService:ListaVacantesService, private router:Router) {
+  }
+  ngOnInit() {
+    this.vacanteService.obtenerVacante(this.vacanteService.getVacante()).subscribe({
+      next: data =>{
+        this.detalleVacante = data;
+      },
+      error: error => {
+        console.error('Error al obtener la vacante ', error);
+      }
+    })
+  }
+  redirigir(){
+    this.router.navigate(['postulacionVacante']);
+  }
+  volver(){
+    this.router.navigate(['listaVacantes']);
+  }
+}
---
+++ FrontendDBD/frontend/src/app/services/servicereclutamiento/detalle-candidato.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {DetalleCandidato} from '../../models/modelsreclutamiento/detalleCandidato.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DetalleCandidatoService {
+  private apiUrl='http://localhost:8080/candidatos';
+  aprobado=true;
+  telefono=0;
+  constructor(private http: HttpClient) { }
+  obtenerCandidatosAprobados(): Observable<DetalleCandidato[]> {
+    return this.http.get<DetalleCandidato[]>(`${this.apiUrl}/aprobados`);
+  }
+  obtenerCandidatosDesaprobados(): Observable<DetalleCandidato[]> {
+    return this.http.get<DetalleCandidato[]>(`${this.apiUrl}/desaprobados`)
+  }
+  cambiarEstado(){
+    this.aprobado=!this.aprobado;
+  }
+  getEstado(){
+    return this.aprobado;
+  }
+  setTelefono(telefono:number){
+    this.telefono = telefono;
+  }
+  getTelefono(){
+    return this.telefono;
+  }
+}
