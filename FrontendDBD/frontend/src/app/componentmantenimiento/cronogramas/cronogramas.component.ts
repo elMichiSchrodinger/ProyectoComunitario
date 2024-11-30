e900d8b1876bb18168889094841d489b4d7150bd
Angel Daniel Quispe Condori <angel.quispe.c@uni.pe>
2024-11-30 15:54:03 -0500

    routes

---
+++ FrontendDBD/frontend/src/app/componentmantenimiento/cronogramas/cronogramas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cronogramas',
+  standalone: true,
+  imports: [],
+  templateUrl: './cronogramas.component.html',
+  styleUrl: './cronogramas.component.css'
+})
+export class CronogramasComponent {
+
+}
