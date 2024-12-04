442a3cb69cd4ce53f604542d222b0b8430b1c1ca
Deiby Albert Garcia Yzique <deiby.garcia.y@uni.pe>
2024-12-04 10:40:35 -0500

    Revert "Merge branch 'main' of https://github.com/elMichiSchrodinger/ProyectoComunitario"
    This reverts commit a20cb738bbf120bae3554f557852a7864e1f0255, reversing
    changes made to 2889902619668621d7d3ecc916a785b536fd39c3.

--- BackendDBD/src/main/java/uni/edu/pe/modulo_crm/service/Mantenimientoservice/CronogramasService.java
+++ BackendDBD/src/main/java/uni/edu/pe/modulo_crm/service/Mantenimientoservice/CronogramasService.java
@@ -5,6 +5,7 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import uni.edu.pe.modulo_crm.dto.Mantenimientodto.Cronograma;
 import uni.edu.pe.modulo_crm.dto.Mantenimientodto.ListaCronogramas;
 
 import java.util.List;
